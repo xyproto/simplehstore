b41f1672826070467c1a0648b3c2a6219bcd9acc
Alexander Rødseth <rodseth@gmail.com>
2015-01-25 03:03:07 +0100

    Refactoring

--- db.go
+++ db.go
@@ -9,8 +9,6 @@ import (
 	"strings"
 )
 
-var Verbose = false
-
 type Host struct {
 	db     *sql.DB
 	dbname string
@@ -32,14 +30,13 @@ type (
 const (
 	// Version number. Stable API within major version numbers.
 	Version = 1.0
+
 	// The default "username:password@host:port/database" that the database is running at
 	defaultDatabaseServer = ""     // "username:password@server:port/"
 	defaultDatabaseName   = "test" // "main"
 	defaultStringLength   = 42     // using VARCHAR, so this will be expanded up to 65535 characters as needed, unless mysql strict mode is enabled
 	defaultPort           = 3306
 )
-
-/* --- Helper functions --- */
 
 // Test if the local database server is up and running
 func TestConnection() (err error) {
@@ -64,141 +61,7 @@ func TestConnectionHost(connectionString string) (err error) {
 	return err
 }
 
-// Split a string into two parts, given a delimiter.
-// Returns the two parts and true if it works out.
-func twoFields(s, delim string) (string, string, bool) {
-	if strings.Count(s, delim) != 1 {
-		return s, "", false
-	}
-	fields := strings.Split(s, delim)
-	return fields[0], fields[1], true
-}
-
 /* --- Host functions --- */
-
-func leftOf(s, delim string) string {
-	if left, _, ok := twoFields(s, delim); ok {
-		return strings.TrimSpace(left)
-	}
-	return ""
-}
-
-func rightOf(s, delim string) string {
-	if _, right, ok := twoFields(s, delim); ok {
-		return strings.TrimSpace(right)
-	}
-	return ""
-}
-
-// Parse a DSN
-func splitConnectionString(connectionString string) (string, string, bool, string, string, string) {
-	var (
-		userPass, hostPortDatabase, dbname       string
-		hostPort, password, username, port, host string
-		hasPassword                              bool
-	)
-
-	// Gather the fields
-
-	// Optional left side of @ with username and password
-	userPass = leftOf(connectionString, "@")
-	if userPass != "" {
-		hostPortDatabase = rightOf(connectionString, "@")
-	} else {
-		if strings.HasSuffix(connectionString, "@") {
-			hostPortDatabase = connectionString[:len(connectionString)-1]
-		} else {
-			hostPortDatabase = connectionString
-		}
-	}
-	// Optional right side of / with database name
-	dbname = rightOf(hostPortDatabase, "/")
-	if dbname != "" {
-		hostPort = leftOf(hostPortDatabase, "/")
-	} else {
-		if strings.HasSuffix(hostPortDatabase, "/") {
-			hostPort = hostPortDatabase[:len(hostPortDatabase)-1]
-		} else {
-			hostPort = hostPortDatabase
-		}
-		dbname = defaultDatabaseName
-	}
-	// Optional right side of : with password
-	password = rightOf(userPass, ":")
-	if password != "" {
-		username = leftOf(userPass, ":")
-	} else {
-		if strings.HasSuffix(userPass, ":") {
-			username = userPass[:len(userPass)-1]
-			hasPassword = true
-		} else {
-			username = userPass
-		}
-	}
-	// Optional right side of : with port
-	port = rightOf(hostPort, ":")
-	if port != "" {
-		host = leftOf(hostPort, ":")
-	} else {
-		if strings.HasSuffix(hostPort, ":") {
-			host = hostPort[:len(hostPort)-1]
-		} else {
-			host = hostPort
-		}
-		if host != "" {
-			port = strconv.Itoa(defaultPort)
-		}
-	}
-
-	if Verbose {
-		log.Println("Connection:")
-		log.Println("\tusername:\t", username)
-		log.Println("\tpassword:\t", password)
-		log.Println("\thas password:\t", hasPassword)
-		log.Println("\thost:\t\t", host)
-		log.Println("\tport:\t\t", port)
-		log.Println("\tdbname:\t\t", dbname)
-		log.Println()
-	}
-
-	return username, password, hasPassword, host, port, dbname
-}
-
-// Build a DSN
-func buildConnectionString(username, password string, hasPassword bool, host, port, dbname string) string {
-
-	// Build the new connection string
-
-	newConnectionString := ""
-	if (host != "") && (port != "") {
-		newConnectionString += "tcp(" + host + ":" + port + ")"
-	} else if host != "" {
-		newConnectionString += "tcp(" + host + ")"
-	} else if port != "" {
-		newConnectionString += "tcp(" + ":" + port + ")"
-		log.Fatalln("There is only a port. This should not happen.")
-	}
-	if (username != "") && hasPassword {
-		newConnectionString = username + ":" + password + "@" + newConnectionString
-	} else if username != "" {
-		newConnectionString = username + "@" + newConnectionString
-	} else if hasPassword {
-		newConnectionString = ":" + password + "@" + newConnectionString
-	}
-	newConnectionString += "/"
-
-	if Verbose {
-		log.Println("DSN:", newConnectionString)
-	}
-
-	return newConnectionString
-}
-
-// Take apart and rebuild the connection string. Also return the dbname.
-func rebuildConnectionString(connectionString string) (string, string) {
-	username, password, hasPassword, hostname, port, dbname := splitConnectionString(connectionString)
-	return buildConnectionString(username, password, hasPassword, hostname, port, dbname), dbname
-}
 
 // Create a new database connection.
 // connectionString may be on the form "username:password@host:port/database".
