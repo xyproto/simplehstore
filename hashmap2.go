b54e146f9ed9b11b49ced0571ad458f2299e934c
Alexander F. Rødseth <rodseth@gmail.com>
2021-12-03 10:49:30 +0100

    Don't use an OwnerSet

--- hashmap2.go
+++ hashmap2.go
@@ -11,7 +11,6 @@ import (
 // Each value is a JSON data blob and can contains sub-keys.
 type HashMap2 struct {
 	dbDatastructure        // KeyValue is .host *Host + .table string
-	ownerTable      string // Set of owner keys
 	seenPropTable   string // Set of all encountered property keys
 }
 
@@ -26,11 +25,6 @@ func NewHashMap2(host *Host, name string) (*HashMap2, error) {
 	if err != nil {
 		return nil, err
 	}
-	// ownerSet is a set of all stored owners/IDs
-	ownerSet, err := NewSet(host, name+"_set_of_all_IDs")
-	if err != nil {
-		return nil, err
-	}
 	// seenPropSet is a set of all encountered property keys
 	seenPropSet, err := NewSet(host, name+"_encountered_property_keys")
 	if err != nil {
@@ -38,7 +32,6 @@ func NewHashMap2(host *Host, name string) (*HashMap2, error) {
 	}
 	hm2.host = host
 	hm2.table = kv.table
-	hm2.ownerTable = ownerSet.table
 	hm2.seenPropTable = seenPropSet.table
 	return &hm2, nil
 }
@@ -46,11 +39,6 @@ func NewHashMap2(host *Host, name string) (*HashMap2, error) {
 // KeyValue returns the *KeyValue of properties for this HashMap2
 func (hm2 *HashMap2) KeyValue() *KeyValue {
 	return &KeyValue{hm2.host, hm2.table}
-}
-
-// OwnerSet returns the owner *Set for this HashMap2
-func (hm2 *HashMap2) OwnerSet() *Set {
-	return &Set{hm2.host, hm2.ownerTable}
 }
 
 // PropSet returns the property *Set for this HashMap2
@@ -104,12 +92,6 @@ func (hm2 *HashMap2) SetMap(owner string, m map[string]string) error {
 		return err
 	}
 
-	// Add the owner to the set
-	if err := hm2.OwnerSet().Add(owner); err != nil {
-		transaction.Rollback()
-		return err
-	}
-
 	// Prepare the changes
 	for k, v := range m {
 		if err := hm2.setPropWithTransaction(ctx, transaction, owner, k, v, checkForFieldSep); err != nil {
@@ -133,7 +115,6 @@ func (hm2 *HashMap2) SetMap(owner string, m map[string]string) error {
 func (hm2 *HashMap2) SetLargeMap(allProperties map[string]map[string]string) error {
 
 	// First get the KeyValue and Set structures that will be used
-	ownerSet := hm2.OwnerSet()
 	kv := hm2.KeyValue()
 	propSet := hm2.PropSet()
 
@@ -144,7 +125,7 @@ func (hm2 *HashMap2) SetLargeMap(allProperties map[string]map[string]string) err
 	recognizedOwners := []string{}
 	unrecognizedOwners := []string{}
 	for owner := range allProperties {
-		if exists, err := ownerSet.Has(owner); exists && err == nil {
+		if exists, err := hm2.Exists(owner); exists && err == nil {
 			recognizedOwners = append(recognizedOwners, owner)
 		} else {
 			unrecognizedOwners = append(unrecognizedOwners, owner)
@@ -219,10 +200,6 @@ func (hm2 *HashMap2) SetLargeMap(allProperties map[string]map[string]string) err
 	}
 	// Then update/insert all unrecognized owners
 	for _, owner := range unrecognizedOwners {
-		// Add the owner to the set of owners in the database
-		if err := ownerSet.Add(owner); err != nil {
-			return err
-		}
 		// Prepare the changes
 		for k, value := range allProperties[owner] {
 			if Verbose {
@@ -290,24 +267,25 @@ func (hm2 *HashMap2) Has(owner, key string) (bool, error) {
 	return true, nil
 }
 
-// Exists checks if a given owner exists as a hash map at all
+// Exists checks if a given owner exists as a hash map at all.
 func (hm2 *HashMap2) Exists(owner string) (bool, error) {
-	found, err := hm2.OwnerSet().Has(owner)
+	// Looking up the owner directly is tricky, but with a property, it's faster.
+	allProps, err := hm2.PropSet().All()
 	if err != nil {
-		// Either an actual error or no result
-		if noResult(err) {
-			return false, nil
-		}
-		// An actual error
 		return false, err
 	}
-	// Got a result, no error
-	return found, nil
+	// TODO: Improve the performance of this by using SQL instead of looping
+	for _, key := range allProps {
+		if found, err := hm2.Has(owner, key); err == nil && found {
+			return true, nil
+		}
+	}
+	return false, nil
 }
 
 // AllWhere returns all owner ID's that has a property where key == value
 func (hm2 *HashMap2) AllWhere(key, value string) ([]string, error) {
-	allOwners, err := hm2.OwnerSet().All()
+	allOwners, err := hm2.All()
 	if err != nil {
 		return []string{}, err
 	}
@@ -346,17 +324,47 @@ func (hm2 *HashMap2) Keys(owner string) ([]string, error) {
 
 // All returns all owner ID's
 func (hm2 *HashMap2) All() ([]string, error) {
-	return hm2.OwnerSet().All()
+	foundOwners := make(map[string]bool)
+	allOwnersAndKeys, err := hm2.KeyValue().All()
+	if err != nil {
+		return []string{}, err
+	}
+	for _, ownerAndKey := range allOwnersAndKeys {
+		if pos := strings.Index(ownerAndKey, fieldSep); pos != -1 {
+			owner := ownerAndKey[:pos]
+			if _, has := foundOwners[owner]; !has {
+				foundOwners[owner] = true
+			}
+		}
+	}
+	keys := make([]string, len(foundOwners))
+	i := 0
+	for k := range foundOwners {
+		keys[i] = k
+		i++
+	}
+	return keys, nil
 }
 
 // Count counts the number of owners for hash map elements
 func (hm2 *HashMap2) Count() (int, error) {
-	return hm2.OwnerSet().Count()
+	a, err := hm2.All()
+	if err != nil {
+		return 0, err
+	}
+	return len(a), nil
+	// return hm2.KeyValue().Count() is not correct, since it counts all owners + fieldSep + keys
+
 }
 
 // CountInt64 counts the number of owners for hash map elements (int64)
 func (hm2 *HashMap2) CountInt64() (int64, error) {
-	return hm2.OwnerSet().CountInt64()
+	a, err := hm2.All()
+	if err != nil {
+		return 0, err
+	}
+	return int64(len(a)), nil
+	// return hm2.KeyValue().Count() is not correct, since it counts all owners + fieldSep + keys
 }
 
 // DelKey removes a key of an owner in a hashmap (for instance the email field for a user)
@@ -368,22 +376,20 @@ func (hm2 *HashMap2) DelKey(owner, key string) error {
 
 // Del removes an element (for instance a user)
 func (hm2 *HashMap2) Del(owner string) error {
-	ownerSet := hm2.OwnerSet()
-	items, err := ownerSet.All()
+	allProps, err := hm2.PropSet().All()
 	if err != nil {
 		return err
 	}
-	for _, v := range items {
-		if v == owner {
-			return ownerSet.Del(v)
+	for _, key := range allProps {
+		if err := hm2.KeyValue().Del(owner + fieldSep + key); err != nil {
+			return err
 		}
 	}
-	return fmt.Errorf("could not find %s", owner)
+	return nil
 }
 
 // Remove this hashmap
 func (hm2 *HashMap2) Remove() error {
-	hm2.OwnerSet().Remove()
 	hm2.PropSet().Remove()
 	if err := hm2.KeyValue().Remove(); err != nil {
 		return fmt.Errorf("could not remove kv: %s", err)
@@ -393,7 +399,6 @@ func (hm2 *HashMap2) Remove() error {
 
 // Clear the contents
 func (hm2 *HashMap2) Clear() error {
-	hm2.OwnerSet().Clear()
 	hm2.PropSet().Clear()
 	if err := hm2.KeyValue().Clear(); err != nil {
 		return err
