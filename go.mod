ec95b77ba644bec2494616774de90998330e3874
Alexander F. Rødseth <rodseth@gmail.com>
2024-12-02 13:16:18 +0100

    Update dependencies

--- go.mod
+++ go.mod
@@ -6,8 +6,8 @@ require (
 	github.com/lib/pq v1.10.9
 	github.com/xyproto/ask v1.0.0
 	github.com/xyproto/cookie/v2 v2.2.3
-	github.com/xyproto/env/v2 v2.0.0
-	github.com/xyproto/pinterface v1.5.3
+	github.com/xyproto/env/v2 v2.5.2
+	github.com/xyproto/pinterface v1.5.4
 	golang.org/x/crypto v0.1.0
 )
 
--- vendor/github.com/xyproto/env/v2/env.go
+++ vendor/github.com/xyproto/env/v2/env.go
@@ -2,7 +2,9 @@
 package env
 
 import (
+	"os"
 	"path/filepath"
+	"sort"
 	"strconv"
 	"strings"
 	"time"
@@ -77,6 +79,11 @@ func Has(envName string) bool {
 	return Str(envName) != ""
 }
 
+// No returns true if the given environment variable name is not set or empty.
+func No(envName string) bool {
+	return Str(envName) == ""
+}
+
 // Is returns true if the given environment variable is the given string value.
 // The whitespace of both values are trimmed before the comparison.
 func Is(envName, value string) bool {
@@ -126,6 +133,15 @@ func Int8(envName string, defaultValue int8) int8 {
 		return defaultValue
 	}
 	return int8(i8)
+}
+
+// UInt returns the number stored in the environment variable, or the provided default value.
+func UInt(envName string, defaultValue uint) uint {
+	ui, err := strconv.ParseUint(Str(envName), 10, 64)
+	if err != nil {
+		return defaultValue
+	}
+	return uint(ui)
 }
 
 // UInt64 returns the number stored in the environment variable, or the provided default value.
@@ -220,16 +236,27 @@ func Contains(envName string, value string) bool {
 	return strings.Contains(Str(envName), value)
 }
 
-// AsBool can be used to interpret a string value as either true or false. Examples of true values are "yes" and "1".
-func AsBool(s string) bool {
+// True checks if the given string is likely to be interpreted as a "true" value
+func True(s string) bool {
 	switch s {
 	case "1", "ABSOLUTELY", "AFFIRMATIVE", "Absolutely", "Affirmative", "ENABLE", "ENABLED", "Enable", "Enabled", "POSITIVE", "Positive", "T", "TRUE", "True", "Y", "YES", "Yes", "absolutely", "affirmative", "enable", "enabled", "positive", "t", "true", "y", "yes":
 		return true
-	case "", "0", "BLANK", "Blank", "DENIED", "DISABLE", "DISABLED", "Denied", "Disable", "Disabled", "F", "FALSE", "False", "N", "NEGATIVE", "NIL", "NO", "NOPE", "NULL", "Negative", "Nil", "No", "Nope", "Null", "blank", "denied", "disable", "disabled", "f", "false", "n", "negative", "nil", "no", "nope", "null":
-		fallthrough
-	default:
-		return false
 	}
+	return false
+}
+
+// False checks if the given string is likely to be interpreted as a "false" value
+func False(s string) bool {
+	switch s {
+	case "", "0", "BLANK", "Blank", "DENIED", "DISABLE", "DISABLED", "Denied", "Disable", "Disabled", "F", "FALSE", "False", "N", "NEGATIVE", "NIL", "NO", "NOPE", "NULL", "Negative", "Nil", "No", "Nope", "Null", "blank", "denied", "disable", "disabled", "f", "false", "n", "negative", "nil", "no", "nope", "null":
+		return true
+	}
+	return false
+}
+
+// AsBool can be used to interpret a string value as either true or false. Examples of true values are "yes" and "1".
+func AsBool(s string) bool {
+	return True(s)
 }
 
 // AsBoolSimple can be used to interpret a string value as either true or false. Only "1" is true, anything else is false.
@@ -237,28 +264,32 @@ func AsBoolSimple(s string) bool {
 	return s == "1"
 }
 
+// CurrentUser returns the value of LOGNAME, USER or just the string "user", in that order
+func CurrentUser() string {
+	return StrAlt("LOGNAME", "USER", "user")
+}
+
 // HomeDir returns the path to the home directory of the user, if available.
-// If not available, $LOGNAME or $USER are used to construct a path starting with /home/.
-// If $LOGNAME and $USER are not available, just "/tmp" is returned.
+// If not available, the username is to construct a path starting with /home/.
+// If a username is not available, then "/tmp" is returned.
 // The returned string is what the home directory should have been named, if it would have existed.
 // No checks are made for if the directory exists.
 func HomeDir() string {
-	homeDir, err := userHomeDir()
-	if err != nil {
-		// Use $LOGNAME, $USER or "user", in that order
-		userName := StrAlt("LOGNAME", "USER", "user")
+	if homeDir, err := userHomeDir(); err == nil { // success, use the home directory
+		return homeDir
+	}
+	userName := CurrentUser()
+	switch userName {
+	case "root":
 		// If the user name is "root", use /root
-		if userName == "root" {
-			return "/root"
-		}
-		// If the user name is "user", use either $HOME or /tmp
-		if userName == "user" {
-			return Str("HOME", "/tmp")
-		}
+		return "/root"
+	case "":
+		// If the user name is not available, use either $HOME or /tmp
+		return Str("HOME", "/tmp")
+	default:
 		// Use $HOME if it's available, and a constructed home directory path if not
 		return Str("HOME", "/home/"+userName)
 	}
-	return homeDir
 }
 
 // ExpandUser replaces a leading ~ or $HOME with the path
@@ -273,4 +304,51 @@ func ExpandUser(path string) string {
 		path = strings.Replace(path, "$HOME", HomeDir(), 1)
 	}
 	return path
+}
+
+// Environ returns either the cached environment, or os.Environ()
+func Environ() []string {
+	if useCaching {
+		var xs []string
+		mut.RLock()
+		defer mut.RUnlock()
+		for k, v := range environment {
+			xs = append(xs, k+"="+v)
+		}
+		return xs
+	}
+	return os.Environ()
+}
+
+// Keys returns the all the environment variable names as a sorted string slice
+func Keys() []string {
+	var keys []string
+	if useCaching {
+		mut.RLock()
+		defer mut.RUnlock()
+		for k := range environment {
+			keys = append(keys, k)
+		}
+		sort.Strings(keys)
+		return keys
+	}
+	for _, keyAndValue := range os.Environ() {
+		pair := strings.SplitN(keyAndValue, "=", 2)
+		keys = append(keys, pair[0])
+	}
+	sort.Strings(keys)
+	return keys
+}
+
+// Map returns the current environment variables as a map from name to value
+func Map() map[string]string {
+	if useCaching {
+		return environment
+	}
+	m := make(map[string]string)
+	for _, keyAndValue := range os.Environ() {
+		pair := strings.SplitN(keyAndValue, "=", 2)
+		m[pair[0]] = pair[1]
+	}
+	return m
 }
